e0df527652c3efd25b5f3aaae9a2d14095e38428
Anthony Chu <antchu@microsoft.com>
2020-07-26 22:30:34 -0700

    Add function app

---
+++ sample/api/weather/index.js
@@ -0,0 +1,15 @@
+module.exports = async function (context, req) {
+    const weather = [];
+    const conditions = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+    const now = new Date().getTime();
+    for (let i = 0; i < 5; i++) {
+        weather.push({
+            "date": new Date(now + i * 24 * 60 * 60 * 1000),
+            "temperatureC": Math.floor(Math.random() * 75) - 20,
+            "summary": conditions[Math.floor(Math.random() * conditions.length)]
+        });
+    }
+    context.res = {
+        body: weather
+    };
+}
